4ad1fbd4e96f9cf80afd9ab018ae4b37e8d98cc2
Ph1l-7 <lussier.philippe@gmail.com>
2023-01-11 15:56:26 -0500

    Ajouter button working-ish, Re-Do? Perhaps for a more accurate site.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Bonjour");
+
+function ajouter(){
+    var item=document.getElementById("desc").value;
+    document.getElementById("maListe").append(item);
+
+
+    alert("L'item est bien ajoute");
+}
