8ab06481c99159d56e0816f03ed374bc923b246d
PH1L-7 <lussier.philippe@gmail.com>
2023-01-16 11:57:51 -0500

    Update to .JS, also fix <total> ASAP

--- script.js
+++ script.js
@@ -2,8 +2,33 @@ console.log("Bonjour");
 
 function ajouter(){
     var item=document.getElementById("desc").value;
-    document.getElementById("maListe").append(item);
+    var price=document.getElementById("valeur").value;  //ajouter price
 
 
-    alert("L'item est bien ajoute");
+    let NewP = document.createElement("p");             //create balise <p>
+    let NewS = document.createElement("span")
+    
+    NewS.innerText = price + "$";
+    NewP.innerText = item + " : "                       //write dans le paragraphe
+   
+    document.getElementById("maListe").append(NewP);    //ajouter paragraphe to list
+    NewP.append(NewS);
+
+
+    //update Total
+    var oldPrix = document.getElementById("total").innerText;
+    var newPrix = +oldPrix + +price;                    //"+"var devient numerique <--> Parse.Int(VAR)
+    document.getElementById("total").innerText = newPrix;
+
+
+
+
+
+
+
+
+
+
+
+    //alert("L'item est bien ajoute");
 }
