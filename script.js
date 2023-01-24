50420ea6860c7c804c1b9df89f235ac3f0a82cf2
PH1L-7 <lussier.philippe@gmail.com>
2023-01-24 10:02:14 -0500

    petit commit, ig

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("Bonjour");
-
+compteur=4;
 function ajouter(){
     var item=document.getElementById("desc").value;
     var price=document.getElementById("valeur").value;  //ajouter price
@@ -8,7 +8,7 @@ function ajouter(){
     let NewP = document.createElement("p");             //create balise <p>
     let NewS = document.createElement("span")
     
-    NewS.innerText = price + "$";
+    NewS.innerText = "$" + price ;
     NewP.innerText = item + " : "                       //write dans le paragraphe
    
     document.getElementById("maListe").append(NewP);    //ajouter paragraphe to list
@@ -22,13 +22,26 @@ function ajouter(){
 
 
 
-
-
-
-
-
-
-
+    //Create new hidden input dans forms pour send par courriel
+    newInput=document.createElement("input");
+    newInput.setAttribute("hidden", true);
+    newInput.setAttribute("name", "Item_"+compteur);
+    newInput.setAttribute("value", item);
+    document.forms[0].append(newInput);
+    compteur++;
 
     //alert("L'item est bien ajoute");
+}
+
+
+
+function ajouterEmail(){
+    var email = document.getElementById("email").value;
+    document.forms[0].action = "mailto:"+email;
+    console.log("mail")
+}
+
+function effacer() {
+    document.forms[0].action="";
+    console.log("eff")
 }
